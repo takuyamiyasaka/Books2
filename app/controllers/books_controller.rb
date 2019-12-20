21505a2d75ff700dca4fe64a019c995e8b7574b1
toirai <larsalex8@gmail.com>
2019-12-21 00:25:07 +0900

    [start]5

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,44 @@
+class BooksController < ApplicationController
+  def edit
+  	@book=Book.find(params[:id])
+  end
+
+  def update
+  	book=Book.find(params[:id])
+  	book.update(book_params)
+  	redirect_to book_path(book.id),notice:"You have updated book successfully."
+  end
+
+  def index
+  	@user=current_user
+  	@book=Book.new
+  	@books=Book.all
+  end
+
+  def show
+  	@_book=Book.new
+  	@book=Book.find(params[:id])
+    @user=@book.user
+  end
+  def create
+  	book = Book.new(book_params)
+  	book.user_id = current_user.id
+  	if book.save
+  	   redirect_to book_path(book.id),notice:"You have creatad book successfully."
+    else
+      @book=book
+      @user=current_user
+      @books=Book.all
+      render :index
+  end
+end
+  def destroy
+    @book=Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
+  end
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '662dbd1313a9a5a6cb0d990f1e874596a0898377848b355d92fe05af0cfdb53384f81f608f86f5216c2af23e57fcf57b77fed5a4b542c115aaa3777c8883f279'
